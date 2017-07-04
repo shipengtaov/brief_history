3567010c0d606a6758939f1be736b570e588cb66
shispt <shispt18@gmail.com>
2017-07-04 22:29:18 +0800

    add STATIC_ROOT

--- brief_history/settings.py
+++ brief_history/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 PAGINATION_SETTINGS = {
     'PAGE_RANGE_DISPLAYED': 10,
